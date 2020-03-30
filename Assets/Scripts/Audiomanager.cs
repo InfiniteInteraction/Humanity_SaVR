76e166215a23ea5fd4ffd9cdd3c9e60eed7f1c30
RonnieDS <58788535+RonnieDS@users.noreply.github.com>
2020-03-30 15:46:27 -0400

    Audio
    -Added an Audiomanager object and script.

---
+++ Assets/Scripts/Audiomanager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class Audiomanager : MonoBehaviour
+{
+    public Sounds[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
+
