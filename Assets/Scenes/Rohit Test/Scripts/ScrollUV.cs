7a3d9bdf5539ad1977a770d2e89d4eeaad94ff8f
rohit-lad <rohit-lad@users.noreply.github.com>
2020-03-31 15:07:00 -0700

    Level update
    First level design
    Added various huts in the level
    Enviromental design
    Ambient sound
    New hut prefabs
    Added collider to the glass dome with a force field effect
    Plams fences around the chief's hut
    New windmill

---
+++ Assets/Scenes/Rohit Test/Scripts/ScrollUV.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollUV : MonoBehaviour
+{
+
+    public float WF_speed = 0.75f;
+
+    public Renderer WF_renderer;
+
+    void Start()
+    {
+        WF_renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+
+        float TextureOffset = Time.time * WF_speed;
+        WF_renderer.material.SetTextureOffset("_MainTex", new Vector2(TextureOffset, 0));
+    }
+
+}
