093e66d598fb3c95269840ddeff26aff96ecd9fb
Charnele <30808061+Charnele@users.noreply.github.com>
2020-06-17 09:51:59 -0400

    shotgun Shotting Spread test
    Script to have the shotgun shoot a spread of bullets

---
+++ Assets/Scripts/ShotGun.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGun : MonoBehaviour
+{
+    public int pelletCount; //Amount of pellets fired
+    public float spreadAngle;
+    public GameObject pellet;//bulletPrefab
+    public float pelletFireVel = 1;
+    public Transform BarrelExit;
+    List<Quaternion> pellets;
+
+     void Awake()
+    {
+        pellets = new List<Quaternion>(pelletCount);
+        for(int i = 0; i<pelletCount; i++)
+        {
+            pellets.Add(Quaternion.Euler(Vector3.zero));//EmptySlot
+        }
+
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("Fire1"))
+        {
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        int i = 0;
+       foreach (Quaternion quat in pellets)
+       //for (int i = 0; i< pelletCount; i++)
+
+            pellets[i] = Random.rotation;
+        GameObject p = Instantiate(pellet, BarrelExit.position, BarrelExit.rotation);
+
+        p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+        p.GetComponent<Rigidbody>().AddForce(p.transform.right * pelletFireVel);
+
+           i++;
+
+    }
+
+}
