335a8dc2e8be0374eb1ee35dded5a738b2815a0a
rohit-lad <rohit-lad@users.noreply.github.com>
2020-06-23 18:19:52 -0700

    Splash Screen Update
    Renamed the scene
    Added new script to anchor PS to OVR Camera
    New layer for the particle system in the scene

---
+++ Assets/Scripts/PS_CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PS_CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (player == null)
+            Debug.Log("PS_CameraFollow script can find the player gameobject.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            transform.position = player.position;
+            transform.rotation = player.rotation;
+        }
+
+    }
+}
