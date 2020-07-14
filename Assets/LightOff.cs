c3bed3467d77db08d4e34f341994b2536c2dd864
rohit-lad <rohit-lad@users.noreply.github.com>
2020-07-14 14:12:14 -0700

    Script Update
    Switching to different skybox when player dies

--- Assets/LightOff.cs
+++ Assets/LightOff.cs
@@ -23,7 +23,7 @@ public class LightOff : MonoBehaviour
             t += Time.deltaTime;
             directionalLight.intensity = Mathf.Lerp(0.9f, 0.0f, t/5);
             RenderSettings.reflectionIntensity = Mathf.Lerp(1f,0f,t/5);
-            //RenderSettings.skybox = deathSkybox;
+            RenderSettings.skybox = deathSkybox;
         }
         else
         {
